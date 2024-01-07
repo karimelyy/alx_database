38dae592f20f4ab8e0cc0787b1349027bcbb735d
karimelyy <elkarims30@gmail.com>
2024-01-07 18:08:32 +0100

    commit

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,3 +1,5 @@
 -- This script lists all tables in the specified database
-USE `dbname`;  -- Replace 'dbname' with the actual name of the database
+
+USE `hbtn_test_db_0`;  -- Replace 'hbtn_test_db_0' with the actual name of the database
+
 SHOW TABLES;
