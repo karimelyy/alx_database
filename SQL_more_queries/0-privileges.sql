5a819295e64249a675ea322f6e90f52be83a540c
karimelyy <elkarims30@gmail.com>
2024-01-08 10:04:28 +0100

    commit

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,17 +1,22 @@
+-- Connect to MySQL server
+mysql -hlocalhost -uroot -p <<EOF
+
 -- Create user user_0d_1 if not exists
-echo "CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';" | mysql -hlocalhost -uroot -p
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
 
 -- Grant all privileges to user_0d_1
-echo "GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';" | mysql -hlocalhost -uroot -p
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 
 -- Create user user_0d_2 if not exists
-echo "CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';" | mysql -hlocalhost -uroot -p
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
 
 -- Grant all privileges to user_0d_2
-echo "GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';" | mysql -hlocalhost -uroot -p
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
 
 -- Display privileges for user_0d_1
-echo "SHOW GRANTS FOR 'user_0d_1'@'localhost';" | mysql -hlocalhost -uroot -p
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
 
 -- Display privileges for user_0d_2
-echo "SHOW GRANTS FOR 'user_0d_2'@'localhost';" | mysql -hlocalhost -uroot -p
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
+
+EOF
