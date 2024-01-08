995cd48de10006c3ba09e3e710b8fb4b2a9892c6
karimelyy <elkarims30@gmail.com>
2024-01-08 10:24:30 +0100

    commit

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,13 +1,13 @@
 -- Drop table force_name if it exists
 DROP TABLE IF EXISTS force_name;
 
--- Create table force_name
-CREATE TABLE force_name (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+-- Create table force_name if not exists
+CREATE TABLE IF NOT EXISTS force_name (
+    id INT,
     name VARCHAR(256) NOT NULL
 );
 
--- Insert records into force_name
+-- Insert a record into force_name
 INSERT INTO force_name (name) VALUES
     ('Holberton School'),
     ('Python is cool'),
