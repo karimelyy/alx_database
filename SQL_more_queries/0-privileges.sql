0d6c3d764ba85b7b72d3adf9ee3aabd099819e63
karimelyy <elkarims30@gmail.com>
2024-01-08 10:10:01 +0100

    commit

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,22 +1,5 @@
--- Connect to MySQL server
-mysql -hlocalhost -uroot -p <<EOF
-
--- Create user user_0d_1 if not exists
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-
--- Grant all privileges to user_0d_1
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-
--- Create user user_0d_2 if not exists
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
-
--- Grant all privileges to user_0d_2
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
-
--- Display privileges for user_0d_1
+-- Check privileges for user_0d_1
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 
--- Display privileges for user_0d_2
+-- Check privileges for user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
-
-EOF
