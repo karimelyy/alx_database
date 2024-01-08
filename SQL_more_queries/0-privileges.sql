c20f3e97e21086bc81b8d2d6ef46d324893b224d
karimelyy <elkarims30@gmail.com>
2024-01-08 10:01:11 +0100

    commit

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,17 +1,17 @@
-# Create user user_0d_1 if not exists
+-- Create user user_0d_1 if not exists
 echo "CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';" | mysql -hlocalhost -uroot -p
 
-# Grant all privileges to user_0d_1
+-- Grant all privileges to user_0d_1
 echo "GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';" | mysql -hlocalhost -uroot -p
 
-# Create user user_0d_2 if not exists
+-- Create user user_0d_2 if not exists
 echo "CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';" | mysql -hlocalhost -uroot -p
 
-# Grant all privileges to user_0d_2
+-- Grant all privileges to user_0d_2
 echo "GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';" | mysql -hlocalhost -uroot -p
 
-# Display privileges for user_0d_1
+-- Display privileges for user_0d_1
 echo "SHOW GRANTS FOR 'user_0d_1'@'localhost';" | mysql -hlocalhost -uroot -p
 
-# Display privileges for user_0d_2
+-- Display privileges for user_0d_2
 echo "SHOW GRANTS FOR 'user_0d_2'@'localhost';" | mysql -hlocalhost -uroot -p
