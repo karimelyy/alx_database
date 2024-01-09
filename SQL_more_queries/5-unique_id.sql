66eb2828d965c37f0412cbabb585a9f427480986
karimelyy <elkarims30@gmail.com>
2024-01-09 13:17:52 +0100

    commit

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,16 +1,13 @@
--- Attempt to create table unique_id
-CREATE TABLE unique_id (
+-- Create table unique_id if not exists
+CREATE TABLE IF NOT EXISTS unique_id (
     id INT DEFAULT 1 UNIQUE,
     name VARCHAR(256)
 );
 
--- Ignore the error if the table already exists
-INSERT IGNORE INTO unique_id (name) VALUES
-    ('Holberton School');
-
 -- Attempt to insert a record with a duplicate id
 INSERT INTO unique_id (id, name) VALUES
-    (1, 'Holberton');
+    (1, 'Holberton')
+    ON DUPLICATE KEY UPDATE id = id;
 
 -- Use a placeholder table to display the data
 CREATE TABLE display_data AS SELECT * FROM unique_id;
