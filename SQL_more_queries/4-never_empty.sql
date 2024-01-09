e56a97e3df33775aba3e10fc0c3d716638e654a3
karimelyy <elkarims30@gmail.com>
2024-01-09 13:01:59 +0100

    commit

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -6,23 +6,3 @@ CREATE TABLE id_not_null (
     id INT DEFAULT 1,
     name VARCHAR(256)
 );
-
--- Insert records into id_not_null
-INSERT INTO id_not_null (name) VALUES
-    ('Holberton School'),
-    ('Python is cool'),
-    ('School'),
-    ('Holberton'),
-    ('C is fun');
-
--- Insert another record without specifying id
-INSERT INTO id_not_null (name) VALUES ('Another record');
-
--- Use a placeholder table to display the data
-CREATE TABLE display_data AS SELECT * FROM id_not_null;
-
--- Drop the original table
-DROP TABLE id_not_null;
-
--- Rename the placeholder table to the original table name
-ALTER TABLE display_data RENAME TO id_not_null;
