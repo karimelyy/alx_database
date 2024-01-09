f9d5df319e1cdbd4c0ff151500327486cb0e771a
karimelyy <elkarims30@gmail.com>
2024-01-09 12:44:38 +0100

    commit

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,19 +1,8 @@
--- Drop table id_not_null if it exists
-DROP TABLE IF EXISTS id_not_null;
-
--- Create table id_not_null
-CREATE TABLE id_not_null (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+-- Create table id_not_null if not exists
+CREATE TABLE IF NOT EXISTS id_not_null (
+    id INT DEFAULT 1,
     name VARCHAR(256)
 );
 
 -- Insert records into id_not_null
-INSERT INTO id_not_null (name) VALUES
-    ('Holberton School'),
-    ('Python is cool'),
-    ('Holberton'),
-    ('School'),
-    ('C is fun');
-
--- Attempt to insert without specifying id
-INSERT INTO id_not_null (name) VALUES ('Another record');
+INSERT INTO id_not_null (id, name) VALUES (89, 'Holberton School');
